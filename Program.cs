704e741c6218acd544a1f4f69ff5b33076e09dd1
Kyle Rusby <8283338+RusbyProgramming@users.noreply.github.com>
2018-02-17 17:16:17 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustEatRecruitmentTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                string input = "";
+                if (args.Length == 1)
+                {
+                    input = args[0];
+                }
+                else
+                {
+                    Console.Write("Please enter outcode: ");
+                    input = Console.ReadLine();
+                }
+
+                GetRestraunts(input);
+            }
+        }
+
+        private static Restraunt[] GetRestraunts(string outcode)
+        {
+            RestClient client = new RestClient("https://public.je-apis.com/");
+            RestRequest request = new RestRequest("restaurants", Method.GET);
+
+            request.AddParameter("q", outcode); 
+            request.AddHeader("Accept-Tenant", "uk");
+            request.AddHeader("Accept-Language", "en-GB");
+            request.AddHeader("Authorization", "JE-API-KEY ABCDE123456789");
+            request.AddHeader("Host", "public.je-apis.com");
+
+            IRestResponse response = client.Execute(request);
+            string content = response.Content;
+
+            Console.WriteLine(content);
+            return null;
+        }
+    }
+}
---
+++ Restraunt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustEatRecruitmentTest
+{
+    class Restraunt
+    {
+        public string Name { get; private set; }
+        public double Rating { get; private set; }
+        public string[] FoodTypes { get; private set; }
+        public Restraunt(string name, double rating, string[] foodTypes)
+        {
+            this.Name = name;
+            this.Rating = rating;
+            this.FoodTypes = FoodTypes;
+        }
+    }
+}
